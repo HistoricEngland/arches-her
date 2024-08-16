890e8fa4a7cfc5a80ffb15a946838e9c433f4f77
Rob Oates <rob.oates@historicengland.org.uk>
2024-08-16 10:30:53 +0000

    Remove button role

--- arches_her/media/js/bindings/reports.js
+++ arches_her/media/js/bindings/reports.js
@@ -8,14 +8,11 @@ define([
         update: function (element, valueAccessor, allBindings) {
             var value = ko.utils.unwrapObservable(valueAccessor());
             var sectionName = allBindings.get('sectionName') || '';
-            $(element).attr('role', 'button');
             if (value) { // section is expanded
                 $(element).attr('aria-expanded', 'true');
                 $(element).attr('aria-label', 'Section ' + sectionName + ' expanded');
                 $(element).addClass('fa-angle-double-right');
                 $(element).removeClass('fa-angle-double-up');
-
-
             } else { // collapsed
                 $(element).attr('aria-expanded', 'false');
                 $(element).attr('aria-label', 'Section ' + sectionName + ' collapsed');
