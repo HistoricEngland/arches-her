59c765c8a75a9e19a7fe116219eaaed536b665fd
Rob Oates <rob.oates@historicengland.org.uk>
2024-08-19 15:24:12 +0000

    Refactor report.js

--- arches_her/media/js/bindings/reports.js
+++ arches_her/media/js/bindings/reports.js
@@ -2,26 +2,28 @@ define([
     'jquery',
     'knockout',
 ], function ($, ko) {
-    // apply aria attributes to the element when an observable changes between true and false
+    // Apply aria attributes to the element when an observable changes between true and false
     // e.g. <div data-bind="onReportSectionToggleAria: visible.names, sectionName: '{% trans "Section Name" %}'"></div>
     ko.bindingHandlers.onReportSectionToggleAria = {
         update: function (element, valueAccessor, allBindings) {
-            var value = ko.utils.unwrapObservable(valueAccessor());
-            var sectionName = allBindings.get('sectionName') || '';
-            if (value) { // section is expanded
-                $(element).attr('aria-expanded', 'true');
-                $(element).attr('aria-label', 'Section ' + sectionName + ' expanded');
-                $(element).addClass('fa-angle-double-right');
-                $(element).removeClass('fa-angle-double-up');
-            } else { // collapsed
-                $(element).attr('aria-expanded', 'false');
-                $(element).attr('aria-label', 'Section ' + sectionName + ' collapsed');
-                $(element).removeClass('fa-angle-double-right');
-                $(element).addClass('fa-angle-double-up');
+            const value = ko.utils.unwrapObservable(valueAccessor());
+            const sectionName = allBindings.get('sectionName') || '';
+
+            function setAriaAttributes(isExpanded) {
+                const ariaExpanded = isExpanded ? 'true' : 'false';
+                const ariaLabel = `Section ${sectionName} ${isExpanded ? 'expanded' : 'collapsed'}`;
+                const addClass = isExpanded ? 'fa-angle-double-right' : 'fa-angle-double-up';
+                const removeClass = isExpanded ? 'fa-angle-double-up' : 'fa-angle-double-right';
+
+                $(element).attr('aria-expanded', ariaExpanded);
+                $(element).attr('aria-label', ariaLabel);
+                $(element).addClass(addClass);
+                $(element).removeClass(removeClass);
             }
+
+            setAriaAttributes(value);
         }
     };
-
 
     return;
 });
