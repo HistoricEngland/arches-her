fbf19b2ad2157838c73a39318746daaedef255cd
Rob Oates <rob.oates@historicengland.org.uk>
2024-10-17 10:07:20 +0000

    Define __software_version__ to indicate the deployment version of Keystone

---
+++ arches_her/__init__.py
@@ -0,0 +1,10 @@
+from arches.setup import get_version
+import os
+
+__software_version__ = None
+software_version = os.getenv("SOFTWARE_VERSION", None)
+
+if software_version:
+    software_version_tuple = tuple(item.strip()
+                                   for item in software_version.split(','))
+    __software_version__ = get_version(software_version_tuple)
