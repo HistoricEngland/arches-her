f07321d0aa8f9b0d1a44d71a2e0095ba5c2fe9f5
Rob Oates <rob.oates@historicengland.org.uk>
2024-10-17 13:33:47 +0000

    Log the class and method; use KEYSTONE-SOFTWARE-VERSION env var

--- arches_her/__init__.py
+++ arches_her/__init__.py
@@ -2,7 +2,7 @@ from arches.setup import get_version
 import os
 
 __software_version__ = None
-software_version = os.getenv("SOFTWARE_VERSION", None)
+software_version = os.getenv("KEYSTONE-SOFTWARE-VERSION", None)
 
 if software_version:
     software_version_tuple = tuple(item.strip()
--- arches_her/storage.py
+++ arches_her/storage.py
@@ -1,11 +1,16 @@
 from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
 import logging
- 
+import inspect
+
 logger = logging.getLogger(__name__)
- 
+
+
 class NonStrictManifestStaticFilesStorage(ManifestStaticFilesStorage):
     manifest_strict = False
- 
+
+    def __init__(self):
+        self.class_name = self.__class__.__name__
+
     def hashed_name(self, name, content=None, filename=None):
         """
         Override hashed_name to avoid raising errors when a file is missing.
@@ -13,11 +18,13 @@ class NonStrictManifestStaticFilesStorage(ManifestStaticFilesStorage):
         try:
             # Call the original hashed_name method
             return super().hashed_name(name, content, filename)
+
         except ValueError as e:
             # Log a warning for the missing file and return the original name (no hashing)
-            logger.warning(f"File not found, skipping hash for {name}: {e}")
+            method_name = inspect.currentframe().f_code.co_name
+            logger.warning(f"{self.class_name}.{method_name} - File not found, skipping hash for {name}: {e}")
             return name  # Return the un-hashed name
- 
+
     def post_process(self, paths, dry_run=False, **options):
         """
         Override post_process to handle missing files gracefully.
@@ -27,5 +34,6 @@ class NonStrictManifestStaticFilesStorage(ManifestStaticFilesStorage):
             for result in results:
                 yield result
         except ValueError as e:
-            logger.warning(f"Skipping missing file during post-processing: {e}")
+            method_name = inspect.currentframe().f_code.co_name
+            logger.warning(f"{self.class_name}.{method_name} - Skipping missing file during post-processing: {e}")
             # Continue processing despite missing files
