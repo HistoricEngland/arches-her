348902e40e429486b741a0bdd9c1d6d95cf68ff3
Rob Oates <rob.oates@historicengland.org.uk>
2024-11-01 15:33:02 +0000

    Add Monument, MaritimeVessel and HistoricAircraft classes

---
+++ arches_her/models/base.py
@@ -0,0 +1,12 @@
+from typing import List
+import datetime
+import uuid
+
+class Base(object):
+    def __init__(self, resource_instance_id: uuid.UUID, primary_reference_number: str, heritage_asset_name: str, descriptions: List[str], last_updated: datetime.datetime):
+        self._resource_instance_id = resource_instance_id
+        self.primaryReferenceNumber = primary_reference_number
+        self.heritageAssetName = heritage_asset_name
+        self.descriptions = descriptions
+        self.lastUpdated = last_updated.astimezone(
+            datetime.timezone.utc).isoformat()
--- arches_her/models/maritime_vessel.py
+++ arches_her/models/maritime_vessel.py
@@ -4,7 +4,7 @@ import uuid
 from .base import Base
 
 
-class Monument(Base):
+class MaritimeVessel(Base):
     def __init__(self, resource_instance_id: uuid.UUID, primary_reference_number: str, heritage_asset_name: str, descriptions: List[str], last_updated: datetime.datetime):
         super().__init__(resource_instance_id, primary_reference_number,
                          heritage_asset_name, descriptions, last_updated)
