9a4762f15117777e8772536a5240bb05a83e568a
Rob Oates <rob.oates@historicengland.org.uk>
2025-01-22 13:36:08 +0000

    Only create self.bibliographyFootnoteReference if there is a value

--- arches_her/models/monument_sources.py
+++ arches_her/models/monument_sources.py
@@ -18,12 +18,19 @@ class MonumentSource(object):
         self.sourceNo = source_no
         self.sourceReference = source_reference
         self.dateOfOrigination = parse_date(date_of_origination)
-        self.sourceDigitalObjectIdentifier = source_digital_object_identifier,
-        self.bibliographyFootnoteReference = None,
         self.sourceUrl = source_url
 
         if set_bibliography_reference and (not information_source_title or not statement_of_authority or not date_of_origination):
             self.bibliographyFootnoteReference = "No Source Available"
 
     def __repr__(self):
-        return f"MonumentSource(informationSourceTitle={self.informationSourceTitle}, statementOfAuthority={self.statementOfAuthority}, sourceNo={self.sourceNo}, sourceReference={self.sourceReference}, dateOfOrigination={self.dateOfOrigination}, sourceDigitalObjectIdentifier={self.sourceDigitalObjectIdentifier}, bibliographyFootnoteReference={self.bibliographyFootnoteReference}, sourceUrl={self.sourceUrl})"
+        repr_str = (
+            f"MonumentSource(informationSourceTitle={self.informationSourceTitle}, "
+            f"statementOfAuthority={self.statementOfAuthority}, sourceNo={self.sourceNo}, "
+            f"sourceReference={self.sourceReference}, dateOfOrigination={self.dateOfOrigination}, "
+            f"sourceDigitalObjectIdentifier={self.sourceDigitalObjectIdentifier}"
+        )
+        if hasattr(self, "bibliographyFootnoteReference"):
+            repr_str += f", bibliographyFootnoteReference={self.bibliographyFootnoteReference}"
+        repr_str += f", sourceUrl={self.sourceUrl})"
+        return repr_str
