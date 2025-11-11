5fe1b7be7001e8af1565b7fab80db0942c7678dc
khodgkinson-he <53860784+khodgkinson-he@users.noreply.github.com>
2025-11-11 15:04:20 +0000

    96926 Area - All reports to have the referenced-by scene added

--- arches_her/media/js/views/components/reports/area.js
+++ arches_her/media/js/views/components/reports/area.js
@@ -6,6 +6,7 @@ define([
     'utils/resource',
     'utils/report',
     'views/components/reports/scenes/name',
+    'views/components/reports/scenes/referenced-by',
     'views/components/reports/scenes/assessments',
     'views/components/reports/scenes/images',
     'views/components/reports/scenes/people',
@@ -37,6 +38,7 @@ define([
             self.resource = ko.observable(self.reportMetadata()?.resource);
             self.displayname = ko.observable(ko.unwrap(self.reportMetadata)?.displayname);
             self.activeSection = ko.observable('name');
+            self.resourceinstanceid = ko.observable(params.report?.report_json?.resourceinstanceid);
 
             self.nameDataConfig = {
                 name: 'area',
