6e8bfc2167dfcbe4d6c0ac3a1751dc58f6d5038f
khodgkinson-he <53860784+khodgkinson-he@users.noreply.github.com>
2025-11-11 16:15:32 +0000

    96927 Artefact - All reports to have the referenced-by scene added

--- arches_her/media/js/views/components/reports/artefact.js
+++ arches_her/media/js/views/components/reports/artefact.js
@@ -6,6 +6,7 @@ define([
     'utils/resource',
     'utils/report',
     'views/components/reports/scenes/name',
+    'views/components/reports/scenes/referenced-by',
     'views/components/reports/scenes/json',
     'views/components/reports/scenes/archive'
 ], function($, _, ko, arches, resourceUtils, reportUtils) {
@@ -45,6 +46,7 @@ define([
             self.resource = ko.observable(self.reportMetadata()?.resource);
             self.displayname = ko.observable(ko.unwrap(self.reportMetadata)?.displayname);
             self.activeSection = ko.observable('name');
+            self.resourceinstanceid = ko.observable(params.report?.report_json?.resourceinstanceid);
 
             self.nameDataConfig = {
                 name: 'artefact',
