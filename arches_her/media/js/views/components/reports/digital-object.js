b1269462767f3e1f2d45f42f19c9ffafb201a1da
khodgkinson-he <53860784+khodgkinson-he@users.noreply.github.com>
2025-11-24 12:11:12 +0000

    96931 Digital Object - All reports to have the referenced-by scene added

--- arches_her/media/js/views/components/reports/digital-object.js
+++ arches_her/media/js/views/components/reports/digital-object.js
@@ -6,6 +6,7 @@ define([
     'utils/resource',
     'utils/report',
     'views/components/reports/scenes/name',
+    'views/components/reports/scenes/referenced-by',
     'views/components/reports/scenes/copyright',
     'views/components/reports/scenes/json'
 ], function($, _, ko, arches, resourceUtils, reportUtils) {
@@ -26,7 +27,8 @@ define([
             self.reportMetadata = ko.observable(params.report?.report_json);
             self.resource = ko.observable(self.reportMetadata()?.resource);
             self.displayname = ko.observable(ko.unwrap(self.reportMetadata)?.displayname);
-            self.activeSection = ko.observable('name');
+            self.activeSection = ko.observable('name');            
+            self.resourceinstanceid = ko.observable(params.report?.report_json?.resourceinstanceid);
 
             self.nameDataConfig = {};
 
